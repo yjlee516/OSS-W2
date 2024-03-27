1e77c32cc0fb9be90fd300110323f23a0660cb23
Cloud710 <163494040+Cloud710@users.noreply.github.com>
2024-03-27 22:31:05 +0900

    Update hello.c
    줄바꿈 추가했습니다

--- hello.c
+++ hello.c
@@ -5,7 +5,7 @@
 
 int main() {
 	printf("Hello World!\n");
-	printf("I'm Yejin Lee, majoring in Computer Science.");
+	printf("I'm Yejin Lee, majoring in Computer Science.\n"); // 줄바꿈 추가했습니다
 =======
   printf("학번 : "); //학번도 남겨주시면 좋을 것 같습니다
 	printf("Bye!");
