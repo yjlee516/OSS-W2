f5758b9246a1ae57c4fe2c9dcf1386b180de9aee
yjlee516 <163550473+yjlee516@users.noreply.github.com>
2024-03-28 17:22:11 +0900

    Update hello.c
    math.h 헤더 파일을 추가한 뒤 sqrt 함수를 사용했습니다.

--- hello.c
+++ hello.c
@@ -2,6 +2,7 @@
 // 방문기록 남기고 갑니다 - 정지원
 
 #include <stdio.h>
+#include <math.h>
 
 int main() {
 	printf("Hello World!\n");
@@ -9,5 +10,7 @@ int main() {
 	printf("I'm Yejin Lee, majoring in Computer Science.\n"); // 줄바꿈 추가했습니다
 	printf("Bye!\n");
 
+	sqrt(2);
+	
 	return 0;
 }
