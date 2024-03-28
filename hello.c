4d0fbc7d24a4719885670b2a7d19774e831bc55f
veun00 <163563719+veun00@users.noreply.github.com>
2024-03-28 15:45:06 +0900

    Update hello.c
    코드를 정리했습니다.

--- hello.c
+++ hello.c
@@ -1,14 +1,13 @@
 // 이예진 202204427 컴퓨터공학전공
 // touched by user2
 // 방문기록 남기고 갑니다 - 정지원
+
 #include <stdio.h>
 
 int main() {
 	printf("Hello World!\n");
 	printf("I'm Yejin Lee, majoring in Computer Science.\n"); // 줄바꿈 추가했습니다
-=======
-  printf("학번 : "); //학번도 남겨주시면 좋을 것 같습니다
+	printf("학번 : "); //학번도 남겨주시면 좋을 것 같습니다
 	printf("Bye!");
-
 	return 0;
 }
