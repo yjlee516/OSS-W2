1fc5c950f06ab787eeb2fc3fbde508682f49cb1e
veun00 <163563719+veun00@users.noreply.github.com>
2024-03-28 16:09:51 +0900

    Update hello.c
    학번 코드를 지웠습니다.

--- hello.c
+++ hello.c
@@ -6,8 +6,6 @@
 int main() {
 	printf("Hello World!\n");
 	printf("I'm Yejin Lee, majoring in Computer Science.\n"); // 줄바꿈 추가했습니다
-=======
-  printf("학번 : "); //학번도 남겨주시면 좋을 것 같습니다
 	printf("Bye!");
 
 	return 0;
