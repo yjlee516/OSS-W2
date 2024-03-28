48d7cdc3a8568e3dead492e27b4ccaa88c159aaa
yjlee516 <163550473+yjlee516@users.noreply.github.com>
2024-03-28 19:11:31 +0900

    Update hello.c
    math.h 헤더 파일을 삭제했습니다.

--- hello.c
+++ hello.c
@@ -2,14 +2,11 @@
 // 방문기록 남기고 갑니다 - 정지원
 
 #include <stdio.h>
-#include <math.h>
 
 int main() {
 	printf("Hello World!\n");
 	printf("I'm Yejin Lee, majoring in Computer Science.\n"); // 줄바꿈 추가했습니다
 	printf("Bye!\n");
 
-	sqrt(3);
-	
 	return 0;
 }
