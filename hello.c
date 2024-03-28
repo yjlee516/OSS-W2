c78d7e03a7ad32e46e4afafdc6be934194da9fd9
yjlee516 <163550473+yjlee516@users.noreply.github.com>
2024-03-28 21:04:21 +0900

    Update hello.c
    코드를 한 줄 추가했습니다.

--- hello.c
+++ hello.c
@@ -6,6 +6,7 @@
 int main() {
 	printf("Hello World!\n");
 	printf("I'm Yejin Lee, majoring in Computer Science.\n"); // 줄바꿈 추가했습니다
+	printf("Thank you for watching.\n");
 	printf("Bye!\n");
 
 	return 0;
